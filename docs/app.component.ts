7a052b460d44115e3d7b9f395e547cb4f1963a7a
hussien shokry <hessinshokry3@gmail.com>
2024-08-27 12:08:57 +0300

    Deploy to GitHub Pages

--- docs/app.component.ts
+++ docs/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "../src/app/header/header.component";
+import { FooterComponent } from "../src/app/footer/footer.component";
 
 @Component({
   selector: 'app-root',
--- docs/app.routes.ts
+++ docs/app.routes.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { AuthorsComponent } from './authors/authors.component';
-import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
-import { BooksComponent } from '../books/books.component';
-import { CategoryDetailsComponent } from './category-details/category-details.component';
-import { AuthorDetailsComponent } from './author-details/author-details.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { BookDetailsComponent } from './book-details/book-details.component'; // Import BookDetailsComponent
+import { LoginComponent } from '../src/app/login/login.component';
+import { AdminComponent } from '../src/app/admin/admin.component';
+import { HomeComponent } from '../src/app/home/home.component';
+import { AboutComponent } from '../src/app/about/about.component';
+import { CategoriesComponent } from '../src/app/categories/categories.component';
+import { AuthorsComponent } from '../src/app/authors/authors.component';
+import { TermsConditionsComponent } from '../src/app/terms-conditions/terms-conditions.component';
+import { BooksComponent } from '../src/app/books/books.component';
+import { CategoryDetailsComponent } from '../src/app/category-details/category-details.component';
+import { AuthorDetailsComponent } from '../src/app/author-details/author-details.component';
+import { UserProfileComponent } from '../src/app/user-profile/user-profile.component';
+import { BookDetailsComponent } from '../src/app/book-details/book-details.component'; // Import BookDetailsComponent
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },

810a895cda6f8bcd7b7b704f929b752886af9690
hussien shokry <hessinshokry3@gmail.com>
2024-08-31 01:35:43 +0300

    delete errors

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -4,11 +4,11 @@ import { ReadListComponent } from '../read-list/read-list.component';
 import { CurrentlyReadingComponent } from '../currently-reading/currently-reading.component';
 import { CategoriesComponent } from '../categories/categories.component';
 import { CategoryDetailsComponent } from '../category-details/category-details.component';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [BooksListComponent, ReadListComponent, CurrentlyReadingComponent, CategoriesComponent, CategoryDetailsComponent],
+  imports: [BooksListComponent, ReadListComponent, CurrentlyReadingComponent, CategoriesComponent, CategoryDetailsComponent,CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
